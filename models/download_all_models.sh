61e9cbec8b10806b35af589d487099c89e3c0282
David Katz <david.katz@clearpoint.co.nz>
2017-04-05 17:04:59 +1200

    add bin bash

--- models/download_all_models.sh
+++ models/download_all_models.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 MODELS=("apple2orange" "facades_photo2label" "map2sat" "orange2apple" "style_cezanne" "style_ukiyoe"  "summer2winter_yosemite" "zebra2horse" "facades_label2photo" "horse2zebra" "monet2photo" "sat2map" "style_monet" "style_vangogh" "winter2summer_yosemite")
 for MODEL in "${MODELS[@]}"
 	do echo $MODEL
